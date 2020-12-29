8c62d671be9db3e3fef953809e7781046d1d0fdf
gabboraron <gabboraron@gmail.com>
2020-12-29 21:12:10 +0100

    gy4 - saját jegyzet
    fogyasztó termelő, signal ping pong

---
+++ gy4/gy4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace gy4
+{
+    class Program
+    {
+        public static int[] numbers = new int[1000000];
+        public static int db1;
+        public static int db2;
+        static void Main(string[] args)
+        {
+            //szekvenciális
+            Random rnd = new Random(1000);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(10);
+            }
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int db = counter(numbers,0, 999999);
+            sw.Stop();
+            Console.WriteLine("=== SZEKVENCIALISAN ===");
+            Console.WriteLine("Tömb mérete: " + numbers.Length);
+            Console.WriteLine("5-ösök darabszáma: " + db);
+            Console.WriteLine("idő (ms): " + sw.ElapsedMilliseconds);
+
+            Console.Write("\n\n");
+            //párhuzamos
+            sw.Start();
+            Thread t1 = new Thread(ThreadMethod1);
+            Thread t2 = new Thread(ThreadMethod2);
+            db = db1 + db2;
+            sw.Stop();
+            Console.WriteLine("=== PÁRHUZAMOSAN ===");
+            Console.WriteLine("Szálak száma: " + 2);
+            Console.WriteLine("5-ösök darabszáma: " + db);
+            Console.WriteLine("idő (ms): " + sw.ElapsedMilliseconds);
+        }
+        private static void ThreadMethod1()
+        {
+             db1 = counter(numbers, 0, 500000);
+        }
+        private static void ThreadMethod2()
+        {
+            db2 = counter(numbers, 500000, 1000000);
+        }
+        private static int counter(int[] numbers,int startIDX,int endIDX)
+        {
+            int counter = 0;
+            for (int i = startIDX; i < endIDX; i++)
+            {
+                if (numbers[i] == 5)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ gy4/signal_pingpong/signal_pingpong/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+
+namespace signal_pingpong
+{
+    class Program
+    {
+        public static int ball;
+        static void Main(string[] args)
+        {
+            new Thread ping = Thread(PingProc);
+            new Thread pong = Thread(Pong);
+            Console.WriteLine("Hello World!");
+        }
+
+        static void PingProc()
+        {
+            lock (ball)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Ping");
+                    Monitor.Pulse(ball);
+                    Monitor.Wait(ball);
+                }
+            }
+        }
+        static void PongProc()
+        {
+            lock (ball)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.WriteLine("Pong");
+                    Monitor.Pulse(ball);
+                    Monitor.Wait(ball);
+                }
+            }
+        }
+    }
+}
